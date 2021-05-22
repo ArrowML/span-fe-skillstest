9018d61c53afd3fd38df26497aff596ed57a4d3d
Marc <marclauder@gmail.com>
2021-05-22 21:02:02 +0200

    initial commit

---
+++ src/components/vHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+      
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'vHeader',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/vNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+      
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'vNav',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
